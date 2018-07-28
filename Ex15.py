701084d066864b72b06a95cb8dec89c659cdc2c8
zzr <627843703@qq.com>
2018-07-28 15:16:10 +0800

    add for and Ex15

---
+++ Ex15.py
@@ -0,0 +1,8 @@
+for i in range(11):
+    print (float(i)/10+1)**(float(i)/10+1)," "
+print 
+for i in range(10,21,1):
+    print (float(i)/10)**(float(i)/10)," "
+#float t[]
+#for i in range(11):
+#    t[i]=1+0.1*i
---
+++ for.py
@@ -0,0 +1,9 @@
+a=(1,2,3)
+for i in a:
+    print a[i-1]
+    print 1+3
+    
+a1={1,2,3}
+a2={1:'a',2:'b',3:'c'}
+for i in a1:
+    print a2[i]
